e7077bad00dbf95751788210b0cd06ac84430c45
JP_Janitha <s219302799@deakin.edu.au>
2022-12-05 18:32:30 +1100

    ok

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,49 +1,86 @@
+// 
 const cardList = [
-    {
-        title: "Hacker 2",
-        image: "images/hacker3.png",
-        link: "About hacker 2",
-        desciption: "Demo desciption about hacker 2"
-    },
-    {
-        title: "Hacker 3",
-        image: "images/hacker.jpg",
-        link: "About hacker 3",
-        desciption: "Demo desciption about hacker 3"
-    }
-]
+  {
+    title: "Kitten 2",
+    image: "images/kitten-2.jpg",
+    link: "About Kitten 2",
+    desciption: "Demo desciption about kitten 2",
+  },
+  {
+    title: "Kitten 3",
+    image: "images/kitten-3.jpg",
+    link: "About Kitten 3",
+    desciption: "Demo desciption about kitten 3",
+  },
+];
+const clickMe = () => {
+  alert("Thanks for clicking me. Hope you have a nice day!");
+};
 
 const submitForm = () => {
-    let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+  let formData = {};
+  formData.title = $("#title").val();
+  formData.image = $("#image").val();
+  formData.link = $("#link").val();
+  formData.description = $("#description").val();
 
-    console.log("Form Data Submitted: ", formData);
-}
+  console.log("Form Data Submitted: ", formData);
+  addProjectToApp(formData);
+};
+
+// ajax functions
+const addProjectToApp = (project) => {
+  $.ajax({
+    url: "/api/projects",
+    data: project,
+    type: "POST",
+    success: (result) => {
+      alert(result.message);
+      location.reload();
+    },
+  });
+};
 
 const addCards = (items) => {
-    items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-    '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-    '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
-      '</div></div></div>';
-      $("#card-section").append(itemToAppend)
-    });
-}
-
-
-
-$(document).ready(function(){
-    $('.materialboxed').materialbox();
-    $('#formSubmit').click(()=>{
-        submitForm();
-    })
-    addCards(cardList);
-    $('.modal').modal();
+  items.forEach((item) => {
+    let itemToAppend =
+      '<div class="col s4 center-align">' +
+      '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' +
+      item.image +
+      '">' +
+      '</div><div class="card-content">' +
+      '<span class="card-title activator grey-text text-darken-4">' +
+      item.title +
+      '<i class="material-icons right">more_vert</i></span><p><a href="#">' +
+      item.link +
+      "</a></p></div>" +
+      '<div class="card-reveal">' +
+      '<span class="card-title grey-text text-darken-4">' +
+      item.title +
+      '<i class="material-icons right">close</i></span>' +
+      '<p class="card-text">' +
+      item.desciption +
+      "</p>" +
+      "</div></div></div>";
+    $("#card-section").append(itemToAppend);
   });
+};
+
+const getProjects = () => {
+  $.get("/api/projects", (response) => {
+    if (response.statusCode == 200) {
+      addCards(response.data);
+    }
+  });
+};
+
+$(document).ready(function () {
+  $(".materialboxed").materialbox();
+  $("#formSubmit").click(() => {
+    submitForm();
+  });
+  getProjects();
+  $(".modal").modal();
+
+  addProjectToApp(formData);
+});
