95f057e9a654195f8ff12dde39d01b16510a8b78
JP_Janitha <s219302799@deakin.edu.au>
2023-01-06 07:26:35 +1100

    Add an anchor for the trial end sectionnn.

--- server.js
+++ server.js
@@ -98,9 +98,19 @@ app.post("/api/projects", (req, res) => {
   });
 });
 
-var port = process.env.port || 3000;
+var port = process.env.port || 8080;
 
 app.listen(port, () => {
   console.log("App listening to: " + port);
   createCollection("Pets");
 });
+
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+  var firstNumber = parseInt(req.params.firstNumber) 
+  var secondNumber = parseInt(req.params.secondNumber)
+  var result = firstNumber + secondNumber || null
+  if(result == null) {
+    res.json({result: result, statusCode: 400}).status(400)
+  }
+  else { res.json({result: result, statusCode: 200}).status(200) } 
+})
