41d38b5a2b39a805d83b60711c21c2698725f6f3
JP_Janitha <s219302799@deakin.edu.au>
2023-01-25 19:49:22 +1100

    Socket it working

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ var app = express();
 var cors = require("cors");
 let projectCollection;
 
+let http = require("http").createServer(app);
+let io = require("socket.io")(http);
+
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,26 +39,10 @@ const uri =
   "mongodb+srv://janitha:GVfy9OvEJ1yMbzfF@cluster0.pxyaxvc.mongodb.net/?retryWrites=true&w=majority";
 const client = new mongoClient(uri, { useNewUrlParser: true });
 
-const createCollection = (collectionName) => {
-  client.connect((err, db) => {
-    projectCollection = client.db().collection(collectionName);
 
-    if (!err) {
-      console.log("MongoDB connected");
-    } else {
-      console.log("MongoDB connection error");
-      process.exit(1);
-    }
-  });
-};
-
-const insertProjects = (project, callback) => {
-  projectCollection.insertOne(project, callback);
-};
-
-const getProjects = (callback) => {
-  projectCollection.find({}).toArray(callback);
-};
+// const getProjects = (callback) => {
+//   projectCollection.find({}).toArray(callback);
+// };
 
 const cardList = [
   {
@@ -72,45 +59,28 @@ const cardList = [
   },
 ];
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
 
-app.get("/api/projects", (req, res) => {
-  getProjects((err, result) => {
-    if (err) {
-      res.json({ statusCode: 400, message: err });
-    } else {
-      res.json({ statusCode: 200, data: result, message: "Success" });
-    }
-  });
-});
-
-app.post("/api/projects", (req, res) => {
-  const newProject = req.body;
-  insertProjects(newProject, (err, result) => {
-    console.log("A new project is added:", newProject);
-    if (err) {
-      res.json({ statusCode: 400, message: err });
-    } else {
-      res.json({ statusCode: 200, data: result, message: "Success" });
-    }
-  });
-});
 
 var port = process.env.port || 8080;
 
-app.listen(port, () => {
-  console.log("App listening to: " + port);
-  createCollection("Pets");
+
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  setInterval(() => {
+    socket.emit("number", parseInt(Math.random() * 10));
+  }, 1000);
 });
 
-app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
-  var firstNumber = parseInt(req.params.firstNumber) 
-  var secondNumber = parseInt(req.params.secondNumber)
-  var result = firstNumber + secondNumber || null
-  if(result == null) {
-    res.json({result: result, statusCode: 400}).status(400)
-  }
-  else { res.json({result: result, statusCode: 200}).status(200) } 
-})
+var port = process.env.port || 3000;
+
+http.listen(port, () => {
+  console.log("App listening to: " + port);
+});
+
+
+
